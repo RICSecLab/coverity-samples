d02de9ba563885c90f0b92f151804dc5a6dda9a2
msymt <nam.nam580@gmail.com>
2022-07-01 10:52:52 +0900

    fix: using cov-build

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 # compile c code
 for file in ./src/*.c
 do
-  gcc -Wall -std=c11 -o ./build/$(basename $file .c) $file
+  cov-build --dir ./build/$(basename $file .c) gcc -Wall -std=c11 $file
 done
 
 # compile c++ code
 for file in ./src/*.cpp
 do
-  g++ -Wall -std=c++11 -o ./build/$(basename $file .cpp) $file
+  cov-build --dir ./build/$(basename $file .cpp) g++ -Wall -std=c++11 $file
 done
