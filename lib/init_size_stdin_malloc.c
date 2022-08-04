ad1cb3dc6f61088ee1031911d53e12176ff9f4ce
msymt <nam.nam580@gmail.com>
2022-08-04 17:08:22 +0900

    fix: disable integer overflow and not checking zero case

--- lib/init_size_stdin_malloc.c
+++ lib/init_size_stdin_malloc.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 void init_size_stdin_malloc() {
-  size_t size = 10;
-  scanf("%ld", &size);
-  if(size < 1) exit(1); // exit if size is less than 1
-  long *arr = malloc(size * sizeof(long));
-  for(int i = 0; i < (int)size; i++) {
-    scanf("%ld", &arr[i]);
+  int size = 10;
+  scanf("%d", &size);
+  int int_size = sizeof(int);
+
+  // check integer overflow
+  // case: size > 0, int_size > 0
+  if(size > INT_MAX/int_size) {
+    perror("integer overflow");
+    exit(1);
+  }
+  // case: size < 0
+  if(size < 0) {
+    perror("invalid size");
+    exit(1);
+  }
+
+  int* arr = (int *)malloc(size * int_size);
+  if(!arr) {
+    perror("malloc");
+    exit(1);
   }
   for(int i = 0; i < (int)size; i++) {
-    printf("%ld\n", arr[i]);
+    arr[i] = i;
+  }
+  for(int i = 0; i < (int)size; i++) {
+    printf("%d\n", arr[i]);
   }
   free(arr);
-  
 }
